7cfe745df2eb480314871206136f48b2ecabbeca
IstkharXrobot <lloda3857@gmail.com>
2025-12-16 18:35:25 +0530

    Update config.py

--- config.py
+++ config.py
@@ -20,8 +20,8 @@ MONGO_DB_URI = getenv("MONGO_DB_URI", None)
 DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 50000))
 
 # Chat id of a group for logging bot's activities
-LOG_GROUP_ID = int(getenv("LOGGER_ID = -1001234567890))
-
+LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", -1002060224175))
+LOGGER_ID = LOG_GROUP_ID
 # Get this value from @MissRose_Bot on Telegram by /id
 OWNER_ID = int(getenv("OWNER_ID", None))
 
